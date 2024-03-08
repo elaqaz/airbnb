e45513250d59f12c1d5b623f2e75f861d97473b1
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-03-08 22:28:48 +0000

    npm install eslint

---
+++ src/App..jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <h1>My App</h1>
+            </div>
+        );
+    }
+}
+ 
+export default App;
