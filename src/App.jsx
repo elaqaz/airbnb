1b1a00eb91deb6305c19ea47dbad868e39134a46
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-03-12 13:33:25 +0000

    make live work

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+    }
+    export default App; 
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+// index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './style.css'; 
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'));
